be98f0fbdcf42609086ffcb828385bdfa79936a7
Louis <louis.fowler@gmail.com>
2020-04-29 13:47:24 +1200

    add new functions to colour divs

--- classes.js
+++ classes.js
@@ -1,25 +1,39 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener("DOMContentLoaded", start);
 
-function start () {
+function start() {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one();
+  two();
+  three();
+  makeVisible();
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
+function one() {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById("one");
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add("blue");
 }
 
 // CREATE FUNCTION two HERE
-
+const two = () => {
+  let getId = document.getElementById("two");
+  getId.classList.add("green");
+};
 // CREATE FUNCTION three HERE
 
+const three = () => {
+  let three = document.getElementById("three");
+  three.classList.add("brown");
+};
+
 // CREATE FUNCTION makeVisible HERE
+
+const makeVisible = () => {
+  document.getElementsByClassName("invisible")[0].style.display = "block";
+};
